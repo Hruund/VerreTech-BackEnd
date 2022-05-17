817f03208857764032c033a964b770f7cf571e0f
root <root@vps-47f6e8f8.vps.ovh.net>
2022-05-17 16:39:40 +0000

    Set default var for db

--- database/database_utils.js
+++ database/database_utils.js
@@ -3,10 +3,10 @@ const res = require('express/lib/response');
 dotenv.config();
 const mysql = require('mysql2')
 const db = mysql.createConnection({
-    host: process.env.HOSTNAME_DDB,
-    user: process.env.USERNAME_DDB,
-    password: process.env.PASSWORD_DDB,
-    database: process.env.DATABASE_DDB
+    host: process.env.HOSTNAME_DDB || 54.36.191.244,
+    user: process.env.USERNAME_DDB || "back",
+    password: process.env.PASSWORD_DDB || "VerreTech@2021",
+    database: process.env.DATABASE_DDB || "verretech"
 });
 
 function executeRequest(sql, callback) {
