51036471189173895016b7f45b98bd981918457e
root <root@vps-47f6e8f8.vps.ovh.net>
2022-05-17 14:18:49 +0000

    reset

--- cart.js
+++ cart.js
@@ -1,6 +1,5 @@
 const dotenv = require('dotenv');
-//dotenv.config();
-dotenv.config({ path: process.cwd() + '/.env' });
+dotenv.config();
 const express = require('express')
 const app = express()
 const port = process.env.PORT_CART
